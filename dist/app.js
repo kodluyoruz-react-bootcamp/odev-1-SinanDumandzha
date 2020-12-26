946d746a56b055de75e987bdd33978390da58cda
Sinan Dumandzha <sinan.dumanci@gmail.com>
2020-12-26 11:19:52 +0200

    dist files removed.

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _servicejs = require('./lib/service.js'); var _servicejs2 = _interopRequireDefault(_servicejs);
-
-async function getUserDataAndUsersPosts(userId) {
-    const getUserData = await _servicejs2.default.call(void 0, userId);
-
-    console.log(getUserData);
-}
-
-getUserDataAndUsersPosts(3);
